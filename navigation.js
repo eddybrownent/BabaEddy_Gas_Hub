05bf42f5cde36d9408e58e7499fb5fbbc0d66ac2
edwin karanja mburu <ekaranja.28@gmail.com>
2024-02-23 00:32:33 +0300

    added new components(screens)

--- navigation.js
+++ navigation.js
@@ -1,10 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Stack = createNativeStackNavigator();
-
-import { View, Text } from 'react-native';
-import React from 'react';
 import ProductsScreen from './screens/ProductsScreen';
 import HomeScreen from './screens/HomeScreen';
 import CartScreen from './screens/CartScreen';
@@ -14,21 +10,25 @@ import LoginScreen from './screens/LoginScreen';
 import SignupScreen from './screens/SignupScreen';
 import ConfirmationScreen from './screens/ConfirmationScreen';
 
+const Stack = createNativeStackNavigator();
+
 export default function Navigation() {
-    return (
-      <NavigationContainer>
-          <Stack.Navigator screenOptions={{
-            headerShown: false
-          }}>
-              <Stack.Screen name="Home" component={HomeScreen} />
-              <Stack.Screen name="Products" component={ProductsScreen} />
-              <Stack.Screen name="Cart" options={{presentation: 'modal'}}component={CartScreen} />
-              <Stack.Screen name="Login" component={LoginScreen} />
-              <Stack.Screen name="Signup" component={SignupScreen} />
-              <Stack.Screen name="OrderPreparing" options={{presentation: 'modal'}}component={OrderPreparing} />
-              <Stack.Screen name="Confirmation" options={{ presentation: 'modal' }} component={ConfirmationScreen} />
-              <Stack.Screen name="DeliveryScreen" options={{presentation: 'modal'}}component={DeliveryScreen} />
-          </Stack.Navigator>
-      </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Products" component={ProductsScreen} />
+        <Stack.Screen name="Cart" options={{ presentation: 'modal' }} component={CartScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="OrderPreparing" options={{ presentation: 'modal' }} component={OrderPreparing} />
+        <Stack.Screen name="Confirmation" options={{ presentation: 'modal' }} component={ConfirmationScreen} />
+        <Stack.Screen name="DeliveryScreen" options={{ presentation: 'modal' }} component={DeliveryScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
