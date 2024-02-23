388fb20f30f930272e8957758814d4aa92a33271
edwin karanja mburu <ekaranja.28@gmail.com>
2024-02-23 23:04:42 +0300

    The bussiness description and the products properties ie price, name..

---
+++ constansts/index.js
@@ -0,0 +1,62 @@
+export const featured = {
+  id: 1,
+  title: 'BabaEddyGas Hub',
+  description: 'Gas Delivery service',
+  image: require('../assets/images/LPG.png'),
+  shop: {
+    id: 1,
+    name: 'BabaEddy Mini Mart',
+    image: require('../assets/images/LPG.png'),
+    description: 'Discover a wide range of high-quality gas products available. From cooking gas cylinders to accessories and more, we offer a diverse selection to meet all your gas needs. Easily place your order and enjoy reliable delivery right to your doorstep.',
+    lng: 37.06817234281969,
+    lat: -1.0389145224029308,
+    address: 'Sec 9, Thika Kenya',
+    stars: 4,
+    reviews: '4.4k',
+    category: 'Gas delivery',
+  },
+  products: [
+    {
+      id: 1,
+      name: '13 Kg Gas',
+      image: require('../assets/images/13kg_gas.png'),
+      price: 2400,
+    },
+    {
+      id: 2,
+      name: '6 Kg Gas',
+      image: require('../assets/images/6kg_gas.png'),
+      price: 1300,
+    },
+    {
+      id: 3,
+      name: 'Meko Grill',
+      image: require('../assets/images/meko_grill.png'),
+      price: 400,
+    },
+    {
+      id: 4,
+      name: 'Gas Burner',
+      image: require('../assets/images/Gas_burner.png'),
+      price: 700,
+    },
+    {
+      id: 5,
+      name: 'Meko Regulator',
+      image: require('../assets/images/Meko_regulator.png'),
+      price: 850,
+    },
+    {
+      id: 6,
+      name: 'L.P Regulator',
+      image: require('../assets/images/Gas_regulator.png'),
+      price: 1500,
+    },
+    {
+      id: 7,
+      name: 'Hose Pipe',
+      image: require('../assets/images/Hose.png'),
+      price: 400,
+    },
+  ],
+};
