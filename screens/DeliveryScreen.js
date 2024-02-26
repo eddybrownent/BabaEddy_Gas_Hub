f5c9ab6b62fcb4eed768d2158406035c3deecc57
edwin karanja mburu <ekaranja.28@gmail.com>
2024-02-26 13:05:03 +0300

    Updates the codes structure for easy readerbility and used ESLint to make the code more consistent and avoid bugs

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -1,53 +1,54 @@
-import { View, Text, StatusBar, TouchableOpacity, Image, SafeAreaView, Linking } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native'
-import { themeColors } from '../theme';
-import MapView, {Marker} from 'react-native-maps';
-import * as Icon from "react-native-feather";
+import {
+  View, Text, TouchableOpacity, Image, Linking,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Icon from 'react-native-feather';
+import { useSelector } from 'react-redux';
 import { selectShop } from '../slices/shopSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import { setShop } from '../slices/shopSlice';
-import { emptyCart } from '../slices/cartSlice';
+import { themeColors } from '../theme';
 
 function DeliveryScreen() {
-    const navigation = useNavigation();
-    const dispatch = useDispatch();
-    const shop = useSelector(selectShop)
-    const handlePhoneCall = () => {
-      const phoneNumber = '0722477345'; // phone number to call
-      Linking.openURL(`tel:${phoneNumber}`);
-    };
+  const navigation = useNavigation();
+  const shop = useSelector(selectShop);
 
-    const handleCancel = ()=>{
-      dispatch(emptyCart());
-      navigation.navigate('Home')
-    }
-    return (
+  const handlePhoneCall = () => {
+    const phoneNumber = '0722477345'; // phone number to call
+    Linking.openURL(`tel:${phoneNumber}`);
+  };
+
+  const handleCancel = () => {
+    // Handle order cancellation logic
+    // dispatch(emptyCart());
+    // Navigate back to the home screen
+    navigation.navigate('Home');
+  };
+  return (
         <View className="flex-1">
              <MapView
         initialRegion={{
-            latitude: shop.lat,
-            longitude: shop.lng,
-            latitudeDelta: 0.01,
-            longitudeDelta: 0.01,
-          }} 
+          latitude: shop.lat,
+          longitude: shop.lng,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
           className="flex-1"
           mapType="standard"
         >
-            <Marker 
+            <Marker
                 coordinate={{
-                    latitude: shop.lat,
-                    longitude: shop.lng,
-                }} 
+                  latitude: shop.lat,
+                  longitude: shop.lng,
+                }}
                 title={shop.title}
-                description={"Welcome at Our Shop!"}
+                description={'Welcome at Our Shop!'}
                 pinColor={themeColors.bgColor(1)}
             />
         </MapView>
 
         <View className="rounded-t-3xl -mt-12 bg-white relative">
           <TouchableOpacity className="absolute right-4 top-2">
-            
+
           </TouchableOpacity>
           <View className="flex-row justify-between px-5 pt-10">
               <View>
@@ -58,35 +59,33 @@ function DeliveryScreen() {
               <Image className="h-24 w-24" source={require('../assets/images/d2.gif')} />
           </View>
 
-          <View 
-          style={{backgroundColor: themeColors.bgColor(0.8)}} 
+          <View
+          style={{ backgroundColor: themeColors.bgColor(0.8) }}
           className="p-2 flex-row justify-between items-center rounded-full my-5 mx-2">
-            <View style={{backgroundColor: 'rgba(255,255,255,0.4)'}} className="p-1 rounded-full">
-              <Image style={{backgroundColor: 'rgba(255,255,255,0.4)'}} className="w-16 h-16 rounded-full" source={require('../assets/images/munga.jpg')} />
+            <View style={{ backgroundColor: 'rgba(255,255,255,0.4)' }} className="p-1 rounded-full">
+              <Image style={{ backgroundColor: 'rgba(255,255,255,0.4)' }} className="w-16 h-16 rounded-full" source={require('../assets/images/munga.jpg')} />
             </View>
-            
+
             <View className="flex-1 ml-3">
                 <Text className="text-lg font-bold text-white">John Munga</Text>
                 <Text className="text-white font-semibold">Your Rider</Text>
             </View>
-            <View  className="flex-row items-center space-x-3 mr-3">
+            <View className="flex-row items-center space-x-3 mr-3">
               <TouchableOpacity onPress={handlePhoneCall} className="bg-white p-2 rounded-full">
                 <Icon.Phone fill={themeColors.bgColor(1)} stroke={themeColors.bgColor(1)} strokeWidth="1" />
               </TouchableOpacity>
-              
+
               <TouchableOpacity onPress={handleCancel} className="bg-white p-2 rounded-full">
                 <Icon.X stroke={'red'} strokeWidth="5" />
               </TouchableOpacity>
-              
+
             </View>
-            
+
         </View>
       </View>
-        
-
 
         </View>
-    )
+  );
 }
 
-export default DeliveryScreen
+export default DeliveryScreen;
