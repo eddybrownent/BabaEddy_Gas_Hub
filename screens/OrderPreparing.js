cb34ba6306d3ce2efaa810321c33152daa49c75e
edwin karanja mburu <ekaranja.28@gmail.com>
2024-02-26 13:07:31 +0300

    updated the useeffiect hook to navifgate to the confirmationScreen with the neccessary oderdetails of the user and the products

--- screens/OrderPreparing.js
+++ screens/OrderPreparing.js
@@ -1,24 +1,33 @@
-import { View, Text, StatusBar, Image } from 'react-native'
-import React, { useEffect } from 'react'
-import { useNavigation, useIsFocused} from '@react-navigation/native';
+import { View, Image } from 'react-native';
+import { useEffect } from 'react';
+import { useNavigation, useIsFocused } from '@react-navigation/native';
 import { useSelector } from 'react-redux';
 import { selectCartproducts } from '../slices/cartSlice';
+import { useAuthentication } from '../context/useAuthentication';
 
 export default function OrderPreparing() {
-    const navigation = useNavigation();
-    const isFocused = useIsFocused();
-    const cartItems = useSelector(selectCartproducts);
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+  const cartItems = useSelector(selectCartproducts);
+  const { user } = useAuthentication();
 
-    useEffect(() => {
-      if (isFocused && cartItems.length > 0) {
-          setTimeout(() => {
-              navigation.navigate('DeliveryScreen');
-          }, 3000);
-      }
-  }, [isFocused, navigation]);
+  useEffect(() => {
+    if (isFocused && cartItems.length > 0 && user) {
+      // Fetch user data
+      setTimeout(() => {
+        navigation.navigate('Confirmation', {
+          cartItems,
+          phoneNumber: user.phoneNumber,
+          address: user.address,
+          username: user.username,
+        });
+      }, 3000);
+    }
+  }, [isFocused, navigation, cartItems, user]);
+
   return (
     <View className="flex-1 bg-white justify-center items-center">
       <Image source={require('../assets/images/deli.gif')} />
     </View>
-  )
+  );
 }
