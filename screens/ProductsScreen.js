55d428fa2bd0ec60eaf654f319d511818f3fe69d
edwin karanja mburu <ekaranja.28@gmail.com>
2024-02-26 13:09:49 +0300

    used eslint to make the code more readable and arranged the structre of it which helps in reading and debuging

--- screens/ProductsScreen.js
+++ screens/ProductsScreen.js
@@ -1,56 +1,65 @@
-import { View, Text, StatusBar, ScrollView, Image, TouchableOpacity } from 'react-native'
-import { useNavigation, useRoute } from '@react-navigation/native'
-import * as Icon from "react-native-feather";
-import { themeColors } from '../theme';
-import { featured } from '../constansts';
+import {
+  View, Text, StatusBar, ScrollView, Image, TouchableOpacity,
+} from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import * as Icon from 'react-native-feather';
 import { useDispatch, useSelector } from 'react-redux';
+import { themeColors } from '../theme';
 import CartIcon from '../components/cartIcon';
-import React, { useEffect, useLayoutEffect, useMemo} from 'react'
-import { selectCartproducts, addToCart, removeFromCart, selectCartproductsById } from '../slices/cartSlice';
+import { addToCart, removeFromCart, selectCartproductsById } from '../slices/cartSlice';
 
 function ProductsScreen() {
-    const dispatch = useDispatch();
-    const navigation = useNavigation();
-    const { params } = useRoute();
-    const { shop, products } = params;
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+  const { params } = useRoute();
+  const { shop, products } = params;
 
-    // Select cart products based on the product IDs
-    const getItemQuantityInCart = (productId) => {
-        const itemInCart = useSelector(state => selectCartproductsById(state, productId));
-        return itemInCart ? itemInCart.length : 0;
-    };
+  // Select cart products based on the product IDs
+  const getItemQuantityInCart = (productId) => {
+    const itemInCart = useSelector((state) => selectCartproductsById(state, productId));
+    return itemInCart ? itemInCart.length : 0;
+  };
 
-    const handleIncrease = (productId, name, price, image, description) => {
-        dispatch(addToCart({ id: productId, name, price, image, description }));
-    }
+  const handleIncrease = (productId, name, price, image, description) => {
+    dispatch(addToCart({
+      id: productId, name, price, image, description,
+    }));
+  };
 
-    const handleDecrease = (productId) => {
-        dispatch(removeFromCart({ id: productId }))
-    }
+  const handleDecrease = (productId) => {
+    dispatch(removeFromCart({ id: productId }));
+  };
 
-    return (
-        <View style={{flex: 1}}>
-        <CartIcon shop={shop} />
+  return (
+        <View style={{ flex: 1 }}>
         <StatusBar style="light" />
-        <ScrollView contentContainerStyle={{paddingBottom: 50}}>
+        <ScrollView contentContainerStyle={{ paddingBottom: 50 }}>
             {/* Your ScrollView content */}
             <View className="relative">
                 <Image style={{ width: '100%', height: 200 }} source={require('../assets/images/LPG.png')} />
                 <TouchableOpacity
                     onPress={() => navigation.goBack()}
                     style={{ position: 'absolute', top: 10, left: 10 }}>
-                    <Icon.ArrowLeft strokeWidth={3} stroke={"white"} />
+                    <Icon.ArrowLeft strokeWidth={3} stroke={'white'} />
                 </TouchableOpacity>
             </View>
-            <View style={{ borderTopLeftRadius: 40, borderTopRightRadius: 40, backgroundColor: 'white', marginTop: -60, paddingTop: 20 }}>
+            <View style={{
+              borderTopLeftRadius: 40, borderTopRightRadius: 40, backgroundColor: 'white', marginTop: -60, paddingTop: 20,
+            }}>
                 <View style={{ paddingHorizontal: 20 }}>
                     <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Choose Products</Text>
                     <Text style={{ fontSize: 14, fontWeight: '500', marginTop: 5 }}>Thank you for choosing us! We're delighted to serve you with free home delivery.</Text>
                 </View>
                 <View style={{ padding: 20 }}>
                     {products.map((product) => (
-                        <View key={product.id} style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 20 }}>
-                            <Image source={product.image} style={{ width: 95, height: 95, borderRadius: 10 }} />
+                        <View key={product.id}
+                            style={{
+                              flexDirection: 'row',
+                              alignItems: 'center',
+                              marginBottom: 20,
+                            }}>
+                            <Image source={product.image}
+                            style={{ width: 95, height: 95, borderRadius: 10 }} />
                             <View style={{ flex: 1, marginLeft: 15 }}>
                                 <Text style={{ fontSize: 18, fontWeight: 'bold' }}>{product.name}</Text>
                                 <Text style={{ fontSize: 16 }}>Ksh {product.price}</Text>
@@ -59,13 +68,26 @@ function ProductsScreen() {
                                 <TouchableOpacity
                                     onPress={() => handleDecrease(product.id)}
                                     disabled={(!getItemQuantityInCart(product.id))}
-                                    style={{ backgroundColor: themeColors.bgColor(1), padding: 10, borderRadius: 20 }}>
+                                    style={{
+                                      backgroundColor: themeColors.bgColor(1),
+                                      padding: 10,
+                                      borderRadius: 20,
+                                    }}>
                                     <Icon.Minus strokeWidth={2} height={20} width={20} stroke="white" />
                                 </TouchableOpacity>
-                                <Text style={{ paddingHorizontal: 10 }}>{getItemQuantityInCart(product.id)}</Text>
+                                <Text style={{ paddingHorizontal: 10 }}
+                                >{getItemQuantityInCart(product.id)}</Text>
                                 <TouchableOpacity
-                                    onPress={() => handleIncrease(product.id, product.name, product.price, product.image, product.description)}
-                                    style={{ backgroundColor: themeColors.bgColor(1), padding: 10, borderRadius: 20 }}>
+                                    onPress={() => handleIncrease(product.id,
+                                      product.name,
+                                      product.price,
+                                      product.image,
+                                      product.description)}
+                                    style={{
+                                      backgroundColor: themeColors.bgColor(1),
+                                      padding: 10,
+                                      borderRadius: 20,
+                                    }}>
                                     <Icon.Plus strokeWidth={2} height={20} width={20} stroke="white" />
                                 </TouchableOpacity>
                             </View>
@@ -74,8 +96,9 @@ function ProductsScreen() {
                 </View>
             </View>
         </ScrollView>
+        <CartIcon shop={shop} />
     </View>
-    );
+  );
 }
 
 export default ProductsScreen;
